eb32c116654e7bf3b5d043ae32a7af095d6bbe40
goubo42@163.com <goubo42@163.com>
2026-06-01 16:44:52 +0800

    feat(frontend): 全局字体改用站酷快乐体（俏皮可爱），Fontsource 自托管同源加载

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import '@fontsource/zcool-kuaile/index.css'  // 站酷快乐体（俏皮可爱），Fontsource 自托管
 import App from './App'
 import './index.css'
 
